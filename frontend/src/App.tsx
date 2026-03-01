210c90093f96cea02a40721d2fadaaee87bbdeef
unknown <lucioucha01@gmail.com>
2026-03-01 13:17:37 -0300

    Agregado componente Vercel Analytics en App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LocaleProvider } from "@/contexts/LocaleContext";
+import { Analytics } from "@vercel/analytics/react"; // <-- 1. Importamos Analytics
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -21,6 +22,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics /> {/* <-- 2. Lo agregamos al final de la app */}
       </TooltipProvider>
     </LocaleProvider>
   </QueryClientProvider>
